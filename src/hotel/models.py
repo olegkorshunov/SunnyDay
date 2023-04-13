3f71c319b9b1010ef9a980dda9f63d1b153cbe95
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-04-13 22:55:48 +0300

    model hotel

---
+++ src/hotel/models.py
@@ -0,0 +1,19 @@
+from typing import List
+import uuid
+
+from sqlalchemy import JSON, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database import Base
+
+
+class Hotel(Base):
+    __tablename__ = "hotel"
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[str] = mapped_column(String, nullable=False)
+    services: Mapped[dict[str, list]] = mapped_column(JSON)
+    rooms_quantity: Mapped[int] = mapped_column(Integer)
+    image_id: Mapped[int] = mapped_column(Integer)
+    room: Mapped[List["room"]] = relationship("hotel")
