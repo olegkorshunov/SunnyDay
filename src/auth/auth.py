46c58281d9673539b67755cd79a541d2ecfe0c61
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-04-19 23:48:18 +0300

    typing

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -11,11 +11,11 @@ from src.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
