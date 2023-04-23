5accf74068721eae4a48ce1f4ec301010bebfe1f
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-04-23 11:05:33 +0300

    added delete method

--- src/dao/daobase.py
+++ src/dao/daobase.py
@@ -1,10 +1,12 @@
-from sqlalchemy import insert, select
+from typing import Any
+
+from sqlalchemy import delete, insert, select
 
 from src.database import async_session_maker
 
 
 class DaoBase:
-    model = None
+    model: Any = None
 
     @classmethod
     async def find_one_by_id(cls, model_id: int):
@@ -30,7 +32,16 @@ class DaoBase:
     @classmethod
     async def insert(cls, **data):
         async with async_session_maker() as session:
-            query = insert(cls.model).values(**data).returning(cls.model.id)  # type: ignore
+            query = insert(cls.model).values(**data).returning(cls.model.id)
             result = await session.execute(query)
+            await session.commit()
+            return result
+
+    @classmethod
+    async def delete(cls, **filter_by):
+        async with async_session_maker() as session:
+            stmt = delete(cls.model).where(**filter_by).returning(cls.model)
+            result = await session.execute(stmt)
+            result = result.scalar()
             await session.commit()
             return result
