92d289bd90397bdd32807b6f95f6ce194b0a4003
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-01 17:42:17 +0300

    added "Find in hotel all not booked rooms"

--- src/hotel/room/dao.py
+++ src/hotel/room/dao.py
@@ -1,34 +1,68 @@
 from datetime import date
-from src.hotel.room.models import Room
+
+from sqlalchemy import func, select
+
+from src.booking.models import Booking
 from src.dao.daobase import DaoBase
+from src.database import async_session_maker
+from src.hotel.room.models import Room
 
 
 class DaoRoom(DaoBase[Room]):
     model = Room
 
     @classmethod
-    def get_hotel_rooms(cls, hotel_id: int, date_from: date, date_to: date):
+    async def find_all(cls, hotel_id: int, date_from: date, date_to: date):
         """
-        WITH hotel_by_location
+        Find in hotel all not booked rooms.
+
+        WITH hotel_rooms
             AS (SELECT *
-                FROM   hotel
-                WHERE  Lower(location) LIKE '%al%'),
-            number_of_boocked_room
-            AS (SELECT r.hotel_id      AS hotel_id,
-                        Count(hotel_id) AS number_of_boocked_room
-                FROM   booking AS b
-                        LEFT JOIN room AS r
-                            ON b.room_id = r.id
-                WHERE  ( '2023-05-15' <= date_from
-                        AND date_from <= '2023-06-20' )
-                        OR ( '2023-05-15' <= date_to
-                            AND date_to <= '2023-06-20' )
-                GROUP  BY r.hotel_id)
-        SELECT h.*,
-            ( h.rooms_quantity - COALESCE(n.number_of_boocked_room, 0) ) AS room_left
-        FROM   hotel_by_location AS h
-            LEFT JOIN number_of_boocked_room AS n
-                    ON h.id = n.hotel_id
-        WHERE  h.rooms_quantity - COALESCE(n.number_of_boocked_room, 0) > 0
+                FROM   room
+                WHERE  room.hotel_id = 1),
+            booking_rooms
+            AS (SELECT b.room_id                     AS room_id,
+                        Coalesce(Count(b.room_id), 0) AS booking
+                FROM   hotel_rooms AS hr
+                        left join booking AS b
+                            ON hr.id = b.room_id
+                WHERE  hr.hotel_id = 1
+                        AND ( '2024-08-15' <= date_from
+                            AND date_from <= '2026-12-20' )
+                        OR ( '2024-08-15' <= date_to
+                            AND date_to <= '2026-12-20' )
+                GROUP  BY b.room_id)
+        SELECT hr.*,
+            br.*,
+            Date_part('day', '2026-12-20' :: timestamp - '2024-08-15' ::
+            timestamp) * hr.price    AS total_cost,
+            hr.quantity - br.booking AS rooms_left
+        FROM   hotel_rooms AS hr
+            left join booking_rooms AS br
+                    ON br.room_id = hr.id
+        WHERE  hr.quantity - br.booking > 0
         """
-        pass
+        hotel_rooms = select(Room).where(Room.hotel_id == hotel_id).cte("hotel_rooms")
+        booking_rooms = (
+            select(Booking.room_id.label("room_id"), func.coalesce(func.count(Booking.room_id), 0).label("booking"))
+            .join(hotel_rooms, hotel_rooms.c.id == Booking.id, isouter=True)
+            .where(
+                (hotel_rooms.c.hotel_id == hotel_id)
+                & ((date_from <= Booking.date_from) & (Booking.date_from <= date_to))
+                | (((date_from <= Booking.date_to) & (Booking.date_to <= date_to)))
+            )
+            .group_by(Booking.room_id)
+        ).cte("booking_rooms")
+        query = (
+            select(
+                hotel_rooms,
+                booking_rooms,
+                (func.Date_part("day", date_to - date_from) * hotel_rooms.c.price).label("total_price"),
+                (hotel_rooms.c.quantity - booking_rooms.c.booking).label("rooms_left"),
+            )
+            .join(booking_rooms, booking_rooms.c.room_id == hotel_rooms.c.id, isouter=True)
+            .where(hotel_rooms.c.quantity - booking_rooms.c.booking > 0)
+        )
+        async with async_session_maker() as ssesion:
+            result = await ssesion.execute(query)
+            return result.mappings().all()
--- src/hotel/room/router.py
+++ src/hotel/room/router.py
@@ -1,8 +1,9 @@
 from datetime import date
 
+from src.hotel.room.dao import DaoRoom
 from src.hotel.router import router
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_all_rooms(hotel_id: int, date_from: date, date_to: date):
-    pass
+async def find_all(hotel_id: int, date_from: date, date_to: date):
+    return await DaoRoom.find_all(hotel_id, date_from, date_to)
