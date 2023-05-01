c603fc63a6454972a84c6c12c5f7a81c0ad95ff9
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-01 18:04:59 +0300

    added find by hotel id

--- src/hotel/router.py
+++ src/hotel/router.py
@@ -1,9 +1,10 @@
 from datetime import date
+from typing import Optional
 
 from fastapi import APIRouter
 
 from src.hotel.dao import DaoHotel
-from src.hotel.schemas import SHotelsWithRoomsLeft
+from src.hotel.schemas import Shotel, SHotelsWithRoomsLeft
 
 router = APIRouter(
     prefix="/hotels",
@@ -15,3 +16,8 @@ router = APIRouter(
 async def find_all(location: str, date_from: date, date_to: date):
     result = await DaoHotel.find_all(location, date_from, date_to)
     return result
+
+
+@router.get("id/{hotel_id}", response_model=Optional[Shotel])
+async def get_hotel_by_id(hotel_id: int):
+    return await DaoHotel.find_one_or_none(id=hotel_id)
--- src/hotel/schemas.py
+++ src/hotel/schemas.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel
 
 
-class SHotelsWithRoomsLeft(BaseModel):
+class Shotel(BaseModel):
     id: int
     name: str
     location: str
     services: list[str]
     rooms_quantity: int
     image_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SHotelsWithRoomsLeft(Shotel):
     rooms_left: int  # number of available rooms
 
     class Config:
