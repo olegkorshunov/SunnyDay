90e9b2bb2a7e6605d472499e632984af531a0a41
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-07 00:42:07 +0300

    added redis cache and imatation of hight load

--- src/hotel/router.py
+++ src/hotel/router.py
@@ -1,7 +1,9 @@
+import asyncio
 from datetime import date
 from typing import Optional
 
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
 
 from src.hotel.dao import DaoHotel
 from src.hotel.schemas import SHotel, SHotelsWithRoomsLeft
@@ -12,8 +14,11 @@ router = APIRouter(
 )
 
 
+@cache(expire=120)  # sec
 @router.get("", response_model=list[SHotelsWithRoomsLeft])
 async def get_hotels_by_location_and_time(location: str, date_from: date, date_to: date):
+    # imatation of hight load
+    await asyncio.sleep(5)  # sec
     result = await DaoHotel.find_all(location, date_from, date_to)
     return result
 
