f91a50e0801aebcaabed60f2c8b22f2ae06ab8b8
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-07 00:42:56 +0300

    redis

--- src/config.py
+++ src/config.py
@@ -11,6 +11,8 @@ class Settings(BaseSettings):
     SECRET_KEY_jwt: str
     ALGORITHM: str
 
+    REDIS_URL: str
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
