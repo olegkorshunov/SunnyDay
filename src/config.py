8951c6b6d6b9f9753e55dd889a22d5ddecba7e64
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-07 15:04:08 +0300

    SMTP

--- src/config.py
+++ src/config.py
@@ -13,6 +13,11 @@ class Settings(BaseSettings):
 
     REDIS_URL: str
 
+    SMTP_HOST: str
+    SMTP_PORT: str
+    SMPT_USER: str
+    SMTP_PASS: str
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
