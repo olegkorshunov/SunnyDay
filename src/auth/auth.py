5eb13b3e1ba8ffc21612e0d696cf27548f43dcb6
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-05-08 16:44:29 +0300

    typing

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -11,7 +11,7 @@ from src.exceptions import HttpException
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def verify_password(plain_password, hashed_password) -> bool:
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
