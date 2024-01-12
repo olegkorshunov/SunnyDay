c6f5c6f1a69a5d39033d1c79b3deeacf23e947c0
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2024-01-12 20:55:03 +0300

    fixed tuple error

--- src/exceptions/http_exceptions.py
+++ src/exceptions/http_exceptions.py
@@ -10,38 +10,36 @@ class SunnyDayException(HTTPException):
 
 
 class UserAlredyExis(SunnyDayException):
-    status_code = (status.HTTP_409_CONFLICT,)
-    detail = ("User alredy exist",)
+    status_code = status.HTTP_409_CONFLICT
+    detail = "User alredy exist"
 
 
 class IncorrectEmailOrPassword(SunnyDayException):
-    status_code = (status.HTTP_409_CONFLICT,)
-    detail = ("Incorrect email or password",)
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Incorrect email or password"
 
 
 class AuthTokenExpier(SunnyDayException):
-    status_code = (status.HTTP_401_UNAUTHORIZED,)
-    detail = ("Auth token has expired",)
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Auth token has expired"
 
 
 class TokenAbsent(SunnyDayException):
-    status_code = (status.HTTP_401_UNAUTHORIZED,)
-    detail = ("Token is absent",)
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Token is absent"
 
 
 class IncorrectTokenFormat(SunnyDayException):
-    status_code = (status.HTTP_401_UNAUTHORIZED,)
-    detail = ("Incorrect token format",)
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Incorrect token format"
 
 
 class UserDataNotFound(SunnyDayException):
-    status_code = (status.HTTP_404_NOT_FOUND,)
-    detail = ("User data not found",)
+    status_code = status.HTTP_404_NOT_FOUND
+    detail = "User data not found"
 
 
 class RoomCanNotBeBooked(SunnyDayException):
-    status_code = (status.HTTP_409_CONFLICT,)
-    detail = ("The room can not be booked",)
+    status_code = status.HTTP_409_CONFLICT
+    detail = "The room can not be booked"
 
-
-print(globals())
